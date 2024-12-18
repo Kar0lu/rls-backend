5b0d7404bc17318cfad23eae901a75ab98f5a059
Kac321 <01187312@pw.edu.pl>
2024-12-18 12:32:21 +0000

    fix(models): Fixed wrong time modeling assumption
    When relationship between Device and Reservation was m:1 then one device could not have been reserved more than one time (throughout whole application lifecycle as we are archiving reservations)
    
    Changed relationship to m:n, to allow multiple device reservation

--- rls/backend/models.py
+++ rls/backend/models.py
@@ -2,9 +2,6 @@ import uuid
 from django.db import models
 from django.contrib.auth.models import User
 
-# not needed for now
-#class UserProfile(models.Model):
-#    profile_id = models.OneToOneField(User, on_delete = models.CASCADE)
 
 class Container(models.Model):
     container_id = models.SmallAutoField(primary_key=True)
@@ -13,23 +10,30 @@ class Container(models.Model):
     name = models.CharField(max_length=50)
     init_script = models.FilePathField(path = '/root/deploy_scripts')
     # TODO: ask if users will be able to (specify ct resources)/(view ct resources will be visible to users)
-    
-    
+
+
+class Device(models.Model):
+    device_id = models.UUIDField(primary_key = True, default = uuid.uuid4, editable = False)
+    name = models.CharField(max_length = 150)
+    device_path = models.FilePathField(path='/tools', allow_folders = True, allow_files = False) # TODO: check if this works for directory paths
+
+
 class Reservation(models.Model):
     reservation_id = models.UUIDField(primary_key=True, default = uuid.uuid4, editable = False)
     created_at = models.DateTimeField(auto_now_add=True)
     valid_since = models.DateTimeField()
     valid_until = models.DateTimeField()
     container = models.ForeignKey(Container, on_delete = models.CASCADE)
+    devices = models.ManyToManyField(Device, through = "Device_Reservation")
     user = models.ForeignKey(User, null = True, on_delete = models.SET_NULL)
     root_password = models.CharField(max_length = 20)
 
 
-class Device(models.Model):
-    device_id = models.UUIDField(primary_key = True, default = uuid.uuid4, editable = False)
-    name = models.CharField(max_length = 150)
-    reservation = models.ForeignKey(Reservation, null = True, blank = True, on_delete = models.SET_NULL)
-    device_path = models.FilePathField(path='/tools', allow_folders = True, allow_files = False) # TODO: check if this works for directory paths
+class Device_Reservation(models.Model):
+    device_reservation_id = models.UUIDField(primary_key = True, default = uuid.uuid4, editable = False)
+    device = models.ForeignKey(Device, on_delete = models.CASCADE)
+    reservation = models.ForeignKey(Reservation, on_delete = models.CASCADE)
+
 
 class Dictionary(models.Model):
     dictionary_id = models.UUIDField(primary_key = True, default = uuid.uuid4, editable = False)
