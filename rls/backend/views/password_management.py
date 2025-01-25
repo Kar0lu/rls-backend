191e9b2cbf1d2591794ec345315378ea8d2c0b54
Kac321 <01187312@pw.edu.pl>
2025-01-25 21:27:11 +0100

    fix: fixed changePassword

--- rls/backend/views/password_management.py
+++ rls/backend/views/password_management.py
@@ -2,18 +2,29 @@ from backend.serializers import ChangePasswordSerializer
 from django.contrib.auth import get_user_model
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
-from rest_framework.permissions import IsAdminUser
 
 User = get_user_model()
 
 class ChangePassword(GenericAPIView):
 
     serializer_class = ChangePasswordSerializer
-    permission_classes = [IsAdminUser]
 
     def put(self, request, id):
+        try: is_admin = request.user.is_staff
+        except: is_admin = False
+        try: password = request.data['password']
+        except: password = ""
+        
         new_password = request.data['new_password']
         obj = get_user_model().objects.get(pk=id)
+        if is_admin:
+            obj.set_password(new_password)
+            obj.save()
+            return Response({'success': 'password changed successfully'}, status=200)
+        
+        if not obj.check_password(raw_password=password):
+                return Response({'error': 'password not match'}, status=400)
+        new_password = request.data['new_password']
         obj.set_password(new_password)
         obj.save()
         return Response({'success': 'password changed successfully'}, status=200)
